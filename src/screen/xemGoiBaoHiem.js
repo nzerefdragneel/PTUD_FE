86b2afae42a3c7682fef239ae507662acdb85d10
43DaNang <114561614+43danang@users.noreply.github.com>
2023-12-26 15:07:30 +0700

    Update xemGoiBaoHiem.js

--- src/screen/xemGoiBaoHiem.js
+++ src/screen/xemGoiBaoHiem.js
@@ -1,33 +1,26 @@
-import React, { Component } from "react";
-import { Card, Typography } from "@material-tailwind/react";
-
-import { withRouter } from "../common/with-router";
-import styles from '../css/screen_css/xemGoiBaoHiem.module.scss';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import React, { Component } from 'react';
+import { Card, Typography } from '@material-tailwind/react';
+import GoiBaoHiem from '../components/item.component';
+import { withRouter } from '../common/with-router';
+//import styles from '../css/screen_css/xemGoiBaoHiem.module.scss';
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
-
+import styles from '../css/component_css/item.module.scss';
 const cx = classNames.bind(styles);
 
 const xemGoiBaoHiem = () => {
-  return (
-    <header className={cx('wrapper')}>
-        <header_chung/>
-        <div className={cx('inner')}>
-          <Link to="/lien-he" className={cx('lien-he')}>
-            <div className={cx('login')}>Đăng nhập</div>
-          </Link>
-          <div>
-            
-          </div>
-        </div>
-
-    </header>
-
-
-  );
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <Link to="/lien-he" className={cx('lien-he')}>
+                    <div>Liên hệ</div>
+                </Link>
+                <div className={cx('cac-goi-bao-hiem')}>
+                    <GoiBaoHiem />
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default xemGoiBaoHiem;
-
